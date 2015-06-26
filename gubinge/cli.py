4c61d1e758da573707d30357a2c3d15747e46a0d
Grahame Bowland <grahame@angrygoats.net>
2015-06-27 01:42:16 +0800

    start on message decoding

--- gubinge/cli.py
+++ gubinge/cli.py
@@ -1,7 +1,40 @@
 import asyncio
 import argparse
+import struct
 import os.path
 from socket import gethostname
+
+
+class MessageTruncated(Exception):
+    pass
+
+
+class MessageInvalid(Exception):
+    pass
+
+
+class SSHMessage:
+    def __init__(self, bytes):
+        "bytes may include more than one SSH message; in which case this will parse the first message"
+        if len(bytes) < 4:
+            raise MessageTruncated()
+        self._length, = struct.unpack('>I', bytes[:4])
+        if len(bytes) < self._length:
+            raise MessageTruncated()
+        if self._length < 1:
+            raise MessageInvalid()
+        self._data = bytes[4:4+self._length]
+        print("woot, we have a complete message, len=%d" % (self._length), self._data)
+        self._parse()
+
+    def __len__(self):
+        "length of this message, including the length uint at the front"
+        return 4 + self._length
+
+    def _parse(self):
+        code_byte, = struct.unpack('c', self._data[:1])
+        code = code_byte[0]
+        print("code", code)
 
 
 class SSHAgentConnection:
@@ -9,6 +42,7 @@ class SSHAgentConnection:
         self._client_reader = client_reader
         self._client_writer = client_writer
         self._id = connection_id
+        self._buf = b''
 
     def log(self, *args, **kwargs):
         print("[%d]" % self._id, *args, **kwargs)
@@ -21,8 +55,22 @@ class SSHAgentConnection:
             if not data:
                 break
             self.log(">> got data", data)
-            self._client_writer.write(data)
+            self._buf += data
+            for mesg in self._parse():
+                self._respond(mesg)
         self.log("disconnected")
+
+    def _parse(self):
+        while self._buf:
+            try:
+                mesg = SSHMessage(self._buf)
+            except MessageTruncated:
+                break
+            self._buf = self._buf[len(mesg):]
+            yield mesg
+
+    def _respond(self, mesg):
+        self._client_writer.write(b'squeek')
 
 
 class SSHAgentProxy:
