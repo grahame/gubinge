8260c75179f4d4a4a3150bfd7f2d00e162fe2a51
Grahame Bowland <grahame@angrygoats.net>
2015-06-27 01:05:14 +0800

    works a bit more

--- gubinge/cli.py
+++ gubinge/cli.py
@@ -4,21 +4,43 @@ import os.path
 from socket import gethostname
 
 
+class SSHAgentConnection:
+    def __init__(self, connection_id, client_reader, client_writer):
+        self._client_reader = client_reader
+        self._client_writer = client_writer
+        self._id = connection_id
+
+    def log(self, *args, **kwargs):
+        print("[%d]" % self._id, *args, **kwargs)
+
+    @asyncio.coroutine
+    def handle(self):
+        self.log("connected")
+        while True:
+            data = yield from self._client_reader.read(8192)
+            if not data:
+                break
+            self.log(">> got data", data)
+            self._client_writer.write(data)
+        self.log("disconnected")
+
+
 class SSHAgentProxy:
     def __init__(self, bind_path, proxy_path):
         self._bind_path = bind_path
         self._proxy_path = proxy_path
         self._loop = asyncio.get_event_loop()
+        self._id = 0
+
+    def get_id(self):
+        issue = self._id
+        self._id += 1
+        return issue
 
     @asyncio.coroutine
     def _client_connected(self, client_reader, client_writer):
-        print(">> client_connected", client_reader, client_writer)
-        while True:
-            data = yield from client_reader.read(8192)
-            if not data:
-                break
-            print(">> got data", data)
-            client_writer.write(data)
+        conn = SSHAgentConnection(self.get_id(), client_reader, client_writer)
+        yield from conn.handle()
 
     @asyncio.coroutine
     def serve(self):
@@ -30,7 +52,11 @@ def main():
     args = parser.parse_args()  # noqa
 
     loop = asyncio.get_event_loop()
-    socket_path = os.path.expanduser("~/.gubinge/sock-%s-%d" % (gethostname(), os.getpid()))
+    socket_path = os.path.expanduser("~/.gubinge/sock-%s" % (gethostname()))  # , os.getpid()))
+    try:
+        os.unlink(socket_path)
+    except OSError:
+        pass
     proxy = SSHAgentProxy(socket_path, os.getenv("SSH_AUTH_SOCK"))
     os.environ["SSH_AUTH_SOCK"] = socket_path
 
