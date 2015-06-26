47ab62aabd5956ee5aa16098b7ff4f985c07c637
Grahame Bowland <grahame@angrygoats.net>
2015-06-27 02:07:30 +0800

    add an enum for all the messages

--- gubinge/cli.py
+++ gubinge/cli.py
@@ -3,6 +3,35 @@ import argparse
 import struct
 import os.path
 from socket import gethostname
+from enum import Enum
+
+
+class MessageType(Enum):
+    SSH_AGENTC_REQUEST_RSA_IDENTITIES = 1
+    SSH_AGENTC_RSA_CHALLENGE = 3
+    SSH_AGENTC_ADD_RSA_IDENTITY = 7
+    SSH_AGENTC_REMOVE_RSA_IDENTITY = 8
+    SSH_AGENTC_REMOVE_ALL_RSA_IDENTITIES = 9
+    SSH_AGENTC_ADD_RSA_ID_CONSTRAINED = 24
+    SSH2_AGENTC_REQUEST_IDENTITIES = 11
+    SSH2_AGENTC_SIGN_REQUEST = 13
+    SSH2_AGENTC_ADD_IDENTITY = 17
+    SSH2_AGENTC_REMOVE_IDENTITY = 18
+    SSH2_AGENTC_REMOVE_ALL_IDENTITIES = 19
+    SSH2_AGENTC_ADD_ID_CONSTRAINED = 25
+    SSH_AGENTC_ADD_SMARTCARD_KEY = 20
+    SSH_AGENTC_REMOVE_SMARTCARD_KEY = 21
+    SSH_AGENTC_LOCK = 22
+    SSH_AGENTC_UNLOCK = 23
+    SSH_AGENTC_ADD_SMARTCARD_KEY_CONSTRAINED = 26
+    SSH_AGENT_FAILURE = 5
+    SSH_AGENT_SUCCESS = 6
+    SSH_AGENT_RSA_IDENTITIES_ANSWER = 2
+    SSH_AGENT_RSA_RESPONSE = 4
+    SSH2_AGENT_IDENTITIES_ANSWER = 12
+    SSH2_AGENT_SIGN_RESPONSE = 14
+    SSH_AGENT_CONSTRAIN_LIFETIME = 1
+    SSH_AGENT_CONSTRAIN_CONFIRM = 2
 
 
 class MessageTruncated(Exception):
@@ -33,8 +62,10 @@ class SSHMessage:
 
     def _parse(self):
         code, = struct.unpack('B', self._data[:1])
-        self.code = code
-        print("code", code)
+        try:
+            self.code = MessageType(code)
+        except ValueError:
+            raise MessageInvalid()
 
 
 class SSHAgentConnection:
@@ -70,10 +101,10 @@ class SSHAgentConnection:
             yield mesg
 
     def _respond(self, mesg):
-        if mesg.code == 1:
-            self._client_writer.write(struct.pack('>IBI', 5, 2, 0))
-        elif mesg.code == 11:
-            self._client_writer.write(struct.pack('>IBI', 5, 12, 0))
+        if mesg.code is MessageType.SSH_AGENTC_REQUEST_RSA_IDENTITIES:
+            self._client_writer.write(struct.pack('>IBI', 5, MessageType.SSH_AGENT_RSA_IDENTITIES_ANSWER.value, 0))
+        elif mesg.code == MessageType.SSH2_AGENTC_REQUEST_IDENTITIES:
+            self._client_writer.write(struct.pack('>IBI', 5, MessageType.SSH2_AGENT_IDENTITIES_ANSWER.value, 0))
 
 
 class SSHAgentProxy:
